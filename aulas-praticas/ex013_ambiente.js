5c3e8fc9fbde452d9f30db0d8348d639289581a5
Denis Henrique <dnishenrique@hotmail.com>
2020-09-16 07:57:13 -0300

    Aula 13
    Aula 13

---
+++ aulas-praticas/ex013_ambiente.js
@@ -0,0 +1,21 @@
+var c = 1
+//usando do while
+do {
+    console.log(`Teste ${c}`)
+    c++
+} while (c <= 6)
+
+/* //usando while 
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+} */
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
